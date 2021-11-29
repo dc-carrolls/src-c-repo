3590853cf77e81175d81cc6bb89763d1aa896f0c
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2021-11-29 13:38:11 +0000

    First C++/Assembly Code

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i++) { cout << i;}
+    cout << "Program Complete" << endl;
+}
